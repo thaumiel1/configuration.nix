06fb4b295b236c3213d7e9729231920c4c77904e
thaumiel1 <williamkane562@gmail.com>
2026-03-18 21:31:43 +0000

    update extra install commands

--- pkgs/cherry-studio.nix
+++ pkgs/cherry-studio.nix
@@ -15,7 +15,8 @@ pkgs.appimageTools.wrapType2 {
   inherit pname version src;
 
   extraInstallCommands = ''
-    install -m 444 -D ${appimageContents}/cherry-studio.desktop \
+    install -m 444 -D \
+      ${appimageContents}/usr/share/applications/cherry-studio.desktop \
       $out/share/applications/cherry-studio.desktop
     substituteInPlace $out/share/applications/cherry-studio.desktop \
       --replace 'Exec=AppRun' 'Exec=cherry-studio'
